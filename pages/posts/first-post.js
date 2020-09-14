f1790ce96a8f404347614551f8e69396cc5b3b0b
Singkorn Dhepyasuvan <singkorn.d@trendvg3.com>
2020-09-14 18:09:26 +0700

    Added JWPlayer

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,28 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default function FirstPost() {
     return (
         <>
-            <h1>First Post</h1>
-            <h2>
-                <Link href="/">
-                    <a>Back to home</a>
-                </Link>
-            </h2>
+            <div>
+                <Head>
+                    <title>First Post</title>
+                    <meta property="og:title" content="My page title" key="title" />
+                </Head>
+                <Head>
+                    <meta property="og:title" content="My new title" key="title" />
+                </Head>
+                <Head>
+                    <script src="https://cdn.jwplayer.com/libraries/RFtywljC.js"></script>
+                </Head>
+                <p>Hello world!</p>
+                <h1>First Post</h1>
+                <h2>
+                    <Link href="/">
+                        <a>Back to home</a>
+                    </Link>
+                </h2>
+            </div>
         </>
     )
 }
